b7664e77da613ab3053dda1d6fb30e3533a689e0
tjlinch <tjlinch@gmail.com>
2022-12-02 13:39:24 -0700

    switched to offcanvas nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import dossasLogo from '../assets/images/TYPEFACE_WHITE.png';
+import dossasTypefaceLogo from '../assets/images/TYPEFACE_WHITE.png';
+import dossasLogo from '../assets/images/BLACK CONCEPT.png';
 
 
 const Header = () => {
@@ -8,18 +9,31 @@ const Header = () => {
     <div className='header'>
       <header>
         <Link to='/'>
-          <img src={dossasLogo} alt='Dossas typeface logo' className='headerLogo'></img>
+          <img src={dossasTypefaceLogo} alt='Dossas typeface logo' className='headerLogo'></img>
         </Link>
       </header>
       <nav>
-        <ul>
-          <Link to='/'>
-            <li className='navItem'>Home</li>
-          </Link>
-          <Link to='/about'>
-            <li className='navItem'>About</li>
-          </Link>
-        </ul>
+        
+        <button className='btn btn-light' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasScrolling' aria-controls='offcanvasScrolling'>
+          <img src={dossasLogo} alt='Nav menu' id='offCanvasImg'></img>
+        </button>
+
+        <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasScrollingLabel">Pages</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className='list-group'>
+              <Link to='/'>
+                    <li className='list-group-item' data-bs-dismiss='offcanvas' aria-label='Close'>Home</li>
+              </Link>
+              <Link to='/about'>
+                    <li className='list-group-item' data-bs-dismiss='offcanvas' aria-label='Close'>About</li>
+              </Link>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   )
