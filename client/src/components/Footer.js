24b6343fe010b6e1f2f2593345cdf934f89264a5
tjlinch <tjlinch@gmail.com>
2022-12-05 11:12:21 -0700

    footer icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import soundcloud from '../assets/icons/icons8-soundcloud-48.png'
+import spotify from '../assets/icons/icons8-spotify-48.png'
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className='iconContainer'>
+        <a href='https://soundcloud.com/user-719208712' target='_blank' rel='noreferrer'>
+          <img src={soundcloud} alt='soundcloud logo'></img>
+          <p>SoundCloud</p>
+        </a>
+      </div>
+      <div className='iconContainer'>
+        <a href='https://open.spotify.com/artist/3W3iYHpJy6ez3VrcKNdRSe' target='_blank' rel='noreferrer'>
+          <img src={spotify} alt='spotify logo'></img>
+          <p>Spotify</p>
+        </a>
+      </div>
+      <p className='iconDisclaimer'>*Icons by<a target="_blank" rel="noreferrer" href="https://icons8.com"> Icons8</a></p>
+    </div>
   )
 }
 
