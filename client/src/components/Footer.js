8d235c2cffa1277f508c94f8cbcf15183a3e1e63
tjlinch <tjlinch@gmail.com>
2022-12-06 12:32:24 -0700

    homepage banner

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,13 +8,13 @@ const Footer = () => {
       <div className='iconContainer'>
         <a href='https://soundcloud.com/user-719208712' target='_blank' rel='noreferrer'>
           <img src={soundcloud} alt='soundcloud logo'></img>
-          <p>SoundCloud</p>
+          {/* <p>SoundCloud</p> */}
         </a>
       </div>
       <div className='iconContainer'>
         <a href='https://open.spotify.com/artist/3W3iYHpJy6ez3VrcKNdRSe' target='_blank' rel='noreferrer'>
           <img src={spotify} alt='spotify logo'></img>
-          <p>Spotify</p>
+          {/* <p>Spotify</p> */}
         </a>
       </div>
       <p className='iconDisclaimer'>*Icons by<a target="_blank" rel="noreferrer" href="https://icons8.com"> Icons8</a></p>
