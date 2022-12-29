11db17b1cec6ceb397f306074ca8dece8a7a34d7
tjlinch <tjlinch@gmail.com>
2022-12-28 18:50:07 -0700

    mockup contact form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -9,20 +9,35 @@ const Contact = () => {
         <form className='emailForm'>
           <input
             type='text'
+            className='textInput'
             name='from-name'
             placeholder='Your Name'
+            // value={toSend.from_name}
+            // onChange={handleChange}
           /><br></br>
-          <p>Select services you are interested in</p>
+          <p>Select services you are interested in:</p>
+          {/* multiple select box with more complicated UI */}
+          <select name='services' id='servicesSelect' multiple>
+            <option value='mix'>Mix</option>
+            <option value='master'>Master</option>
+            <option value='soundDesign'>Sound Design</option>
+            <option value ='production'>Production</option>
+            <option value ='composition'>Composition</option>
+            <option value='freelance'>Freelance</option>
+          </select><br></br>
+          {/* checkbox design that takes up more room */}
           <input
             type='checkbox'
             id='mix'
+            className='checkboxInput'
             name='mix'
-            value='Mix'
+            value='Mix' // or value={toSend.mix}?
           />
           <label for='mix'>Mix</label><br></br>
           <input
             type='checkbox'
             id='master'
+            className='checkboxInput'
             name='master'
             value='Master'
           />
@@ -30,6 +45,7 @@ const Contact = () => {
           <input
             type='checkbox'
             id='soundDesign'
+            className='checkboxInput'
             name='soundDesign'
             value='Sound Design'
           />
@@ -37,6 +53,7 @@ const Contact = () => {
           <input
             type='checkbox'
             id='production'
+            className='checkboxInput'
             name='production'
             value='Production'
           />
@@ -44,6 +61,7 @@ const Contact = () => {
           <input
             type='checkbox'
             id='composition'
+            className='checkboxInput'
             name='composition'
             value='Composition'
           />
@@ -51,11 +69,24 @@ const Contact = () => {
           <input
             type='checkbox'
             id='freelance'
+            className='checkboxInput'
             name='freelance'
             value='Freelance'
           />
           <label for='freelance'>Freelance</label><br></br>
-
+          <input
+            type='text'
+            className='textInput'
+            name='reply_to'
+            placeholder='Your Email'
+          /><br></br>
+          <input
+            type='text'
+            className='textInput'
+            name='message'
+            placeholder='Your Message Here'
+          /><br></br>
+          <button type='submit' className='btn btn-primary' id='submitBtn'>Submit Email</button>
         </form>
       </section>
     </div>
